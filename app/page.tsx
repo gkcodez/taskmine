f0c9cf40f41568fd4ad4fb9846b1f962f1361839
gkcodez <gopalakrishnan.work@gmail.com>
2024-12-03 12:18:45 +0530

    Navbar items added

--- app/page.tsx
+++ app/page.tsx
@@ -20,17 +20,22 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex flex-col gap-4 items-center sm:justify-center justify-start">
-      <div className="flex items-center justify-between gap-2 w-full bg-gray-700 p-3">
-        <div></div>
-        <FiCheckSquare className="h-8 w-8 text-white dark:text-sky-400" />
-        <h1 className="w-full font-semibold text-2xl text-white">Taskmine</h1>
-        <Button
-      variant="secondary"
-    >
-      Settings
-    </Button>
+    <main className="flex flex-col gap-4 items-center sm:justify-center justify-start w-full h-full">
+      <div className="flex items-center justify-between gap-2 bg-gray-700 p-3  w-full h-full">
+        <div className="flex items-center justify-center gap-5">
+        <div className="flex items-center justify-center gap-2"> 
+          <FiCheckSquare className="h-8 w-8 text-white dark:text-sky-400" />
+          <h1 className="w-full font-semibold text-2xl text-white">Taskmine</h1>
+        </div>
+        {/* <div className="flex items-center justify-center gap-2">
+          <h3 className="w-full  text-white">Tasks</h3>
+          <h3 className="w-full  text-white">Habits</h3>
+        </div> */}
+        </div>
+        <div className="flex items-center justify-center gap-2">
+        <Button variant="secondary">Settings</Button>
         <SignOutButton />
+        </div>
       </div>
       <div className="flex flex-wrap items-start justify-center gap-2 w-full h-full px-3">
         {/* <div className="flex items-center justify-between gap-4 pb-4 w-full">
