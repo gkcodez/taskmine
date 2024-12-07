8144c29dda652e3c1dfab7575dd3767b56ad2f2b
gkcodez <gopalakrishnan.work@gmail.com>
2024-12-07 19:43:52 +0530

    Dashboard updated

--- app/dashboard/LogoutButton.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { logout } from "./action";
-
-export default function LogoutButton() {
-  return <Button onClick={() => logout()}>Logout</Button>;
-}
